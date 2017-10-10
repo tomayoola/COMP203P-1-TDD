67e71cd7f4efb68e84411bf8beec5183be858977
tomayoola <tom.ayoola@abehr.com>
2017-10-10 12:44:44 +0100

    TDD

--- src/main/java/ucl/cs/Example.java
+++ src/main/java/ucl/cs/Example.java
@@ -1,6 +1,27 @@
 package ucl.cs;
+import java.util.ArrayList;
+
 
 public class Example {
+  public ArrayList<String> list;
+
+  public Example() {
+    list = new ArrayList<String>();
+  }
+
+  public void addItem(String s) {
+    int i = list.indexOf(s);
+    if (i != -1) {
+      list.remove(i);
+    }
+    list.add(0,s);
+  }
+
+  public String getItem(String s) {
+    int i = list.indexOf(s);
+    return list.get(i);
+  }
+
 
   public int answer() {
     return 42;
--- src/test/java/ucl/cs/ExampleTest.java
+++ src/test/java/ucl/cs/ExampleTest.java
@@ -12,4 +12,42 @@ public class ExampleTest {
     assertThat(new Example().answer(), is(42));
   }
 
+  @Test
+  public void whenInitIsEmpty() {
+    assertThat(new Example().list.isEmpty(), is(true));
+  }
+
+  @Test
+  public void canAddItem() {
+    Example example = new Example();
+    int length = example.list.size();
+    example.addItem("NewItem");
+    assertThat(example.list.size(), is(length+1));
+  }
+
+  @Test
+  public void canGetItem() {
+    Example example = new Example();
+    example.addItem("NewItem");
+    assertThat(example.getItem("NewItem"), is("NewItem"));
+  }
+
+  @Test
+  public void mostRecentIsFirst() {
+    Example example = new Example();
+    example.addItem("SecondItem");
+    example.addItem("FirstItem");
+    assertThat(example.list.get(0), is("FirstItem"));
+  }
+
+  @Test
+  public void moveDuplicates() {
+    Example example = new Example();
+    example.addItem("SecondItem");
+    example.addItem("FirstItem");
+    example.addItem("SecondItem");
+    assertThat(example.list.get(0), is("SecondItem"));
+    assertThat(example.list.size(), is(2));
+  }
+
 }
